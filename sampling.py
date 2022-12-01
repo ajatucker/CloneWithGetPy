403b50ac7852f75a5373ea7fd7f289250b85f9fe
alexistucker <ajatucker@outlook.com>
2022-12-01 12:23:19 -0500

    fix duplicates

--- sampling.py
+++ sampling.py
@@ -8,7 +8,7 @@ from unidiff import PatchSet
 from io import StringIO
 import random
 
-sample_size = 378
+sample_size = 379
 sampled_list = []
 
 with open("D:/CloneWithGetPy/ML-SampledCommitsFrom-PythonProjects.csv", "a", newline='', encoding='utf-8') as write_file:
@@ -19,15 +19,20 @@ with open("D:/CloneWithGetPy/ML-SampledCommitsFrom-PythonProjects.csv", "a", new
     export = readDataframe.values.tolist()
 
     while(len(sampled_list) < sample_size):
+        
         row = readDataframe.sample()
+    
         print("Randomly selected row: ")
         print(row)
         row_number = row.index.tolist()
         new_row = [export[row_number[0]][0],export[row_number[0]][1],export[row_number[0]][2],export[row_number[0]][3],export[row_number[0]][4]]
-        #print("Fixing row number: ",row_number[0]+6)
-        print("Inserting row: ",new_row)
-        sampled_list.append(new_row)
-        z.writerow(new_row)
+        if(new_row in sampled_list):
+            print("Already in list.")
+        else:
+            #print("Fixing row number: ",row_number[0]+6)
+            print("Inserting row: ",new_row)
+            sampled_list.append(new_row)
+            z.writerow(new_row)
        # print(len(sampled_list))
 
 write_file.close()
